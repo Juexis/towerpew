3a4d3e6b6abf73956fea7291d7a8e24aafc89114
Juexis <77648644+Juexis@users.noreply.github.com>
2024-03-28 14:11:27 -0400

    Added movement and janky jump

--- towerpew/Assets/Scripts/playerMovement.cs
+++ towerpew/Assets/Scripts/playerMovement.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    float speed = 30;
+    float speed = 4;
+    public float jumpPower;
+    bool isJumping;
     float horizontalInput;
     void Start()
     {
         
     }
 
-    
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        //moves character based on horizontal input
+        rb.velocity = new Vector2(horizontalInput, 0) * speed;
+
+        if (isJumping)
+        {
+            Debug.Log("Jump!");
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            //rb.AddForce(new Vector2(0, 1 * jumpPower), ForceMode2D.Impulse);
+            isJumping = false;
+        }
+
     }
 }
